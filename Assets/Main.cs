af9cdc36aac1c5b146c0899c0ecb6fa35ef07006
Oleksandr Symonenko <osymonenko@amcbridge.com>
2020-03-05 10:12:26 +0200

    the simple client-server message exchange created, few visual changes, auth preparation

---
+++ Assets/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    //public Web web;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Web.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetDate());
+        //StartCoroutine(GetUsers());
+    }
+
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:52052/api/AppConnect/GetTestJson/TESTData", string.Empty))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                //Show results as text
+                Debug.Log(www.downloadHandler.text);
+                //Debug.Log(www.downloadHandler.ToString());
+
+                //Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:52052/api/AppConnect/GetTestJson/TESTUsers"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                //Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+}
